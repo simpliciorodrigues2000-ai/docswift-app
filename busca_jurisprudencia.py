7b632cded3dd3524a492a75b2d06552dd438d62a
simpliciorodrigues2000-ai <simpliciorodrigues2000@gmail.com>
2026-03-16 08:42:23 -0300

    Create busca_jurisprudencia.py
    def buscar_jurisprudencia(tema):
    
        return f"""
    Possíveis entendimentos jurisprudenciais sobre {tema}:
    
    STJ:
    A jurisprudência entende que atos administrativos devem respeitar
    os princípios da legalidade e razoabilidade.
    
    STF:
    O controle judicial é possível quando há violação de direitos
    fundamentais ou ilegalidade administrativa.
    """

---
+++ busca_jurisprudencia.py
@@ -0,0 +1,13 @@
+def buscar_jurisprudencia(tema):
+
+    return f"""
+Possíveis entendimentos jurisprudenciais sobre {tema}:
+
+STJ:
+A jurisprudência entende que atos administrativos devem respeitar
+os princípios da legalidade e razoabilidade.
+
+STF:
+O controle judicial é possível quando há violação de direitos
+fundamentais ou ilegalidade administrativa.
+"""
